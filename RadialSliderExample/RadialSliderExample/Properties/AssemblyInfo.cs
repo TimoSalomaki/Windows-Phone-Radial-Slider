6f6c770b4e552cf0d7f6fd627983e0aaeabdca6d
SubsonicDesign <timo@subsonicdesign.net>
2013-02-02 16:28:26 +0200

    Small change in AssemblyInfo.cs
    Changed the assembly title to a shorter one

--- RadialSliderExample/RadialSliderExample/Properties/AssemblyInfo.cs
+++ RadialSliderExample/RadialSliderExample/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Subsonic Design Radial Slider Example")]
+[assembly: AssemblyTitle("Radial Slider Example")]
 [assembly: AssemblyDescription("An example application for the radial slider Windows Phone control.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Subsonic Design")]
